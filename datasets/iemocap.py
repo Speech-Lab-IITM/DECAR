d1f1d476a6b98cafe34a1a202591297cba7697c4
sandesh katta <sandesh.katta97@gmail.com>
2021-09-13 16:43:48 +0530

    downstream_final

--- datasets/iemocap.py
+++ datasets/iemocap.py
@@ -22,7 +22,7 @@ class IEMOCAPDataset(Dataset):
         self.uttr_labels= pd.read_csv(annotations_file)
         self.transform = transform
         self.sample_rate = sample_rate
-        self.labels_dict = {'ang': 0, 'dis': 1, 'exc': 2, 'fea': 3, 'fru': 4, 'hap': 5, 'neu': 6, 'oth': 7, 'sad': 8, 'sur': 9}
+        self.labels_dict ={'neu':0, 'ang':1, 'sad':2, 'hap':3} #{'ang': 0, 'dis': 1, 'exc': 2, 'fea': 3, 'fru': 4, 'hap': 5, 'neu': 6, 'oth': 7, 'sad': 8, 'sur': 9}
         #DataUtils.map_labels(self.uttr_labels['Label'].to_numpy())
         self.no_of_classes= len(self.labels_dict)
 
@@ -30,8 +30,38 @@ class IEMOCAPDataset(Dataset):
         return len(self.uttr_labels)
 
     def __getitem__(self, idx):
-        path,label = self.uttr_labels.iloc[idx,:]
-        uttr_path = os.path.join(self.feat_root,path)
+        row = self.uttr_labels.iloc[idx,:]
+        uttr_path =os.path.join(self.feat_root,row['Path'])
         uttr_melspec = np.load(uttr_path)
+        label = row['Label']
         return uttr_melspec, self.labels_dict[label]
 
+
+class IEMOCAPDatasetL2(Dataset):
+    def __init__(self, type, 
+                    transform=None,
+                    target_transform=None,
+                    sample_rate=16000):        
+        self.feat_root =  DataUtils.root_dir["IEMOCAP"]
+        if type == "train":
+            annotations_file=os.path.join(self.feat_root,"train_data.csv")
+        elif type == "test":
+            annotations_file=os.path.join(self.feat_root,"test_data.csv")    
+        else:
+            raise NotImplementedError
+        self.uttr_labels= pd.read_csv(annotations_file)
+        self.transform = transform
+        self.sample_rate = sample_rate
+        self.labels_dict ={'neu':0, 'ang':1, 'sad':2, 'hap':3} #{'ang': 0, 'dis': 1, 'exc': 2, 'fea': 3, 'fru': 4, 'hap': 5, 'neu': 6, 'oth': 7, 'sad': 8, 'sur': 9}
+        #DataUtils.map_labels(self.uttr_labels['Label'].to_numpy())
+        self.no_of_classes= len(self.labels_dict)
+
+    def __len__(self):
+        return len(self.uttr_labels)
+
+    def __getitem__(self, idx):
+        row = self.uttr_labels.iloc[idx,:]
+        uttr_path =os.path.join(self.feat_root,row['PathL2'])
+        uttr_melspec = np.load(uttr_path)
+        label = row['Label']
+        return uttr_melspec, self.labels_dict[label]
